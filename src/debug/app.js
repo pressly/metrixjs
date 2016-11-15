1e7e78bcb6a00d1ffbd078ae40d0855ea52df75a
Maciej Lisiewski <maciej.lisiewski@gmail.com>
2016-11-15 17:17:53 -0500

    Adds a log of more events

--- src/debug/app.js
+++ src/debug/app.js
@@ -32,12 +32,12 @@ export { PMX }
 // Payload keys are grabbed from the default payload values fieldset
 /*
 {
-  "key": [
+  'key': [
     'LOGIN',              // module
     'SOCIAL_LOGIN_FAIL',  // event
-    ['network'],          // required fields (from both "standard" and extra)
-    ['hub_id', 'org_id'], // optional fields (from both "standard" and extra)
-    {"key": val},         // example values of the non-standard, extra fields
+    ['network'],          // required fields (from both 'standard' and extra)
+    ['hub_id', 'org_id'], // optional fields (from both 'standard' and extra)
+    {'key': val},         // example values of the non-standard, extra fields
     'post_events',        // table in which the data will be stored - to generate testing query
     'comment',            // extra information
   ]
@@ -52,59 +52,94 @@ export const actions = {
   'Login: SSO login success':['LOGIN', 'SSO_LOGIN_OK', ['object_id'],['hub_id', 'org_id'], {}, 'events', 'object_id should be set to sso_config.id, set org_id when on custom login page, set hub_id when logging in while on a hub'],  
   'Login: social login fail':['LOGIN', 'SOCIAL_LOGIN_FAIL', ['network'],['hub_id', 'org_id'], {'network': 'facebook'}, 'events', 'set org_id when on custom login page, set hub_id when logging in while on a hub'],
   'Login: social login success':['LOGIN', 'SOCIAL_LOGIN_OK', ['network'],['hub_id', 'org_id'], {'network': 'facebook'}, 'events', 'set org_id when on custom login page, set hub_id when logging in while on a hub'],
+  
+  'Feed: view feed':['FEED', 'VIEW', [],[], {}, 'events', ''],
+  'Feed: open post':['FEED', 'open', ['post_id'],[], {}, 'events', ''],
+  'Feed: open hub':['FEED', 'open', ['hub_id'],[], {}, 'events', ''],
+  'Feed: open hub collection':['FEED', 'open', ['hub_id', 'collection_id'],[], {}, 'events', ''],
+  'Feed: like post':['FEED', 'POST_LIKE', ['hub_id', 'post_id'],[], {}, 'post_events', ''],
+  'Feed: unlike post':['FEED', 'POST_UNLIKE', ['hub_id', 'post_id'],[], {}, 'post_events', ''],
+  'Feed: comment post':['FEED', 'POST_COMMENT', ['hub_id', 'post_id', 'object_id'],[], {}, 'post_events', 'set object_id to the id of the comment'],
+  'Feed: delete post comment':['FEED', 'POST_COMMENT_DELETE', ['hub_id', 'post_id', 'object_id'],[], {}, 'post_events', 'set object_id to the id of the comment'],
+  'Feed: post share':['FEED', 'POST_SHARE', ['hub_id', 'post_id', 'object_id', 'network'],[], {'network': 'twitter'}, 'post_events', 'set object_id to the id of the share'],
+  'Feed: schedule post share':['FEED', 'POST_SHARE_SCHEDULE', ['hub_id', 'post_id', 'object_id', 'network'],[], {'network': 'twitter'}, 'post_events', 'set object_id to the id of the share'],
 
-  'Hub: create a hub':['HUB', 'CREATE', ["hub_id"], [], {}, 'events', ''],
-  'Hub: delete a hub':['HUB', 'DELETE', ["hub_id"], [], {}, 'events', ''],
-  'Hub: update a hub':['HUB', 'UPDATE', ["hub_id"], [], {}, 'events', ''],
-
-  'Hub stream: view hub stream':['HUB_STREAM', 'VIEW', ["hub_id"],[], {}, 'post_events', ''],
-  'Hub stream: view hub collection stream':['HUB_STREAM', 'VIEW', ["hub_id", "collection_id"],[], {}, 'post_events', ''],
-  'Hub stream: view hub tag stream':['HUB_STREAM', 'VIEW', ["hub_id", "tags"],[], {'tags': ['my-tag']}, 'post_events', 'tag has to be passed as a single element tags array'],
-  'Hub stream: view hub author stream':['HUB_STREAM', 'VIEW', ["hub_id", "account_id"],[], {}, 'post_events', 'set account_id to author.id'],
-  'Hub stream: click on post':['HUB_STREAM', 'OPEN', ["hub_id", "post_id"],[], {}, 'post_events', ''],
-  'Hub stream: like post':['HUB_STREAM', 'POST_LIKE', ["hub_id", "post_id"],[], {}, 'post_events', ''],
-  'Hub stream: unlike post':['HUB_STREAM', 'POST_UNLIKE', ["hub_id", "post_id"],[], {}, 'post_events', ''],
-  'Hub stream: comment post':['HUB_STREAM', 'POST_COMMENT', ["hub_id", "post_id", 'object_id'],[], {}, 'post_events', 'set object_id to the id of the comment'],
-  'Hub stream: delete post comment':['HUB_STREAM', 'POST_COMMENT_DELETE', ["hub_id", "post_id", 'object_id'],[], {}, 'post_events', 'set object_id to the id of the comment'],
-  'Hub stream: post share':['HUB_STREAM', 'POST_SHARE', ["hub_id", "post_id", 'object_id', 'network'],[], {'network': 'twitter'}, 'post_events', 'set object_id to the id of the share'],
-  'Hub stream: schedule post share':['HUB_STREAM', 'POST_SHARE_SCHEDULE', ["hub_id", "post_id", 'object_id', 'network'],[], {'network': 'twitter'}, 'post_events', 'set object_id to the id of the share'],
+  'Hub stream: view hub index':['HUB_STREAM', 'VIEW', ['hub_id'],[], {}, 'post_events', ''],
+  'Hub stream: view hub collection stream':['HUB_STREAM', 'VIEW', ['hub_id', 'collection_id'],[], {}, 'post_events', ''],
+  'Hub stream: view hub tag stream':['HUB_STREAM', 'VIEW', ['hub_id', 'tags'],[], {'tags': ['my-tag']}, 'post_events', 'tag has to be passed as a single element tags array'],
+  'Hub stream: view hub author stream':['HUB_STREAM', 'VIEW', ['hub_id', 'account_id'],[], {}, 'post_events', 'set account_id to author.id'],
+  'Hub stream: open post':['HUB_STREAM', 'OPEN', ['hub_id', 'post_id'],[], {}, 'post_events', ''],
+  'Hub stream: like post':['HUB_STREAM', 'POST_LIKE', ['hub_id', 'post_id'],[], {}, 'post_events', ''],
+  'Hub stream: unlike post':['HUB_STREAM', 'POST_UNLIKE', ['hub_id', 'post_id'],[], {}, 'post_events', ''],
+  'Hub stream: comment post':['HUB_STREAM', 'POST_COMMENT', ['hub_id', 'post_id', 'object_id'],[], {}, 'post_events', 'set object_id to the id of the comment'],
+  'Hub stream: delete post comment':['HUB_STREAM', 'POST_COMMENT_DELETE', ['hub_id', 'post_id', 'object_id'],[], {}, 'post_events', 'set object_id to the id of the comment'],
+  'Hub stream: post share':['HUB_STREAM', 'POST_SHARE', ['hub_id', 'post_id', 'object_id', 'network'],[], {'network': 'twitter'}, 'post_events', 'set object_id to the id of the share'],
+  'Hub stream: schedule post share':['HUB_STREAM', 'POST_SHARE_SCHEDULE', ['hub_id', 'post_id', 'object_id', 'network'],[], {'network': 'twitter'}, 'post_events', 'set object_id to the id of the share'],
 
   'Hub spotlight: view post':['HUB_SPOTLIGHT', 'VIEW', ['post_id'], ['collection_id', 'tags', 'account_id'], {'tags': ['my-tag1', 'my-tag2']}, 'post_events', ''],
-  'Hub spotlight: like post':['HUB_SPOTLIGHT', 'POST_LIKE', ["hub_id", "post_id"],[], {}, 'post_events', ''],
-  'Hub spotlight: unlike post':['HUB_SPOTLIGHT', 'POST_UNLIKE', ["hub_id", "post_id"],[], {}, 'post_events', ''],
-  'Hub spotlight: comment post':['HUB_SPOTLIGHT', 'POST_COMMENT', ["hub_id", "post_id", 'object_id'],[], {}, 'post_events', 'set object_id to the id of the comment'],
-  'Hub spotlight: delete post comment':['HUB_SPOTLIGHT', 'POST_COMMENT_DELETE', ["hub_id", "post_id", 'object_id'],[], {}, 'post_events', 'set object_id to the id of the comment'],
-  'Hub spotlight: post share':['HUB_SPOTLIGHT', 'POST_SHARE', ["hub_id", "post_id", 'object_id', 'network'],[], {'network': 'twitter'}, 'post_events', 'set object_id to the id of the share'],
-  'Hub spotlight: schedule post share':['HUB_SPOTLIGHT', 'POST_SHARE_SCHEDULE', ["hub_id", "post_id", 'object_id', 'network'],[], {'network': 'twitter'}, 'post_events', 'set object_id to the id of the share'],  
-  /*'Hub: visit hub index': ['HUB', 'VIEW', ['hub_id']],
-  'Hub: visit a collection': ['HUB_STREAM', 'VIEW', ['hub_id', 'collection_id']],
+  'Hub spotlight: like post':['HUB_SPOTLIGHT', 'POST_LIKE', ['hub_id', 'post_id'],[], {}, 'post_events', ''],
+  'Hub spotlight: unlike post':['HUB_SPOTLIGHT', 'POST_UNLIKE', ['hub_id', 'post_id'],[], {}, 'post_events', ''],
+  'Hub spotlight: comment post':['HUB_SPOTLIGHT', 'POST_COMMENT', ['hub_id', 'post_id', 'object_id'],[], {}, 'post_events', 'set object_id to the id of the comment'],
+  'Hub spotlight: delete post comment':['HUB_SPOTLIGHT', 'POST_COMMENT_DELETE', ['hub_id', 'post_id', 'object_id'],[], {}, 'post_events', 'set object_id to the id of the comment'],
+  'Hub spotlight: post share':['HUB_SPOTLIGHT', 'POST_SHARE', ['hub_id', 'post_id', 'object_id', 'network'],[], {'network': 'twitter'}, 'post_events', 'set object_id to the id of the share'],
+  'Hub spotlight: schedule post share':['HUB_SPOTLIGHT', 'POST_SHARE_SCHEDULE', ['hub_id', 'post_id', 'object_id', 'network'],[], {'network': 'twitter'}, 'post_events', 'set object_id to the id of the share'],
+  'Hub spotlight: open next/prev post':['HUB_SPOTLIGHT', 'OPEN', ['hub_id', 'post_id'],[], {}, 'post_events', 'set post_id to the id current post'],
 
-  'Hub: visit team': ['HUB_TEAM', 'VIEW', ['hub_id']],
-  'Hub: new member joins': ['HUB_TEAM', '???', []], // TODO
-  'Hub: invite new member': ['HUB_TEAM', '???', []], // TODO
+  'Hub post: create a post':['HUB', 'CREATE', ['hub_id', 'post_id'], [], {}, 'events', ''],
+  'Hub post: update a post':['HUB', 'UPDATE', ['hub_id', 'post_id'], [], {}, 'events', ''],
+  'Hub post: delete a post':['HUB', 'DELETE', ['hub_id', 'post_id'], [], {}, 'events', ''],  
+  'Hub post: go to source (bypass spotlight)':['EXTERNAL', 'VIEW', ['hub_id', 'post_id'], [], {}, 'events', 'don\'t send it from web or mobile app - leave it to shortstuff'],  
 
-  'Hub: update hub settings': ['HUB_SETTINGS', 'UPDATE', ['hub_id']],
+  'Hub team: view collaborators':['HUB_TEAM_COLLABORATORS', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub team: view members':['HUB_TEAM_MEMBERS', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub team: view invites':['HUB_TEAM_INVITES', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub team: send invite':['HUB_TEAM_INVITES', 'INVITE', ['hub_id', 'account_id', 'invite_id'], [], {}, 'events', ''],
+  'Hub team: send email invite':['HUB_TEAM_INVITES_EMAIL', 'INVITE', ['hub_id', 'account_id', 'invite_id'], [], {}, 'events', ''],
+  'Hub team: leave hub':['HUB_TEAM', 'LEAVE', ['hub_id', 'account_id', 'invite_id'], [], {}, 'events', ''],
+  'Hub team: request access':['HUB_TEAM', 'REQUEST_ACCESS', ['hub_id', 'account_id', 'invite_id'], [], {}, 'events', ''],
+  'Hub team: grant access':['HUB_TEAM', 'GRANT_ACCESS', ['hub_id', 'account_id', 'request_id'], [], {}, 'events', ''],
+  'Hub team: deny access':['HUB_TEAM', 'DENY_ACCESS', ['hub_id', 'account_id', 'request_id'], [], {}, 'events', ''],
+  'Hub team: change_role':['HUB_TEAM', 'CHANGE_ROLE', ['hub_id', 'account_id'], [], {}, 'events', ''],
+    
+  'Hub settings: view profile settings':['HUB_SETTINGS_PROFILE', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings: view custom domain settings':['HUB_SETTINGS_DOMAIN', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings: view advanved settings':['HUB_SETTINGS_ADVANCED', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings: update profile settings':['HUB_SETTINGS_PROFILE', 'UPDATE', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings: update custom domain settings':['HUB_SETTINGS_DOMAIN', 'UPDATE', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings: update advanved settings':['HUB_SETTINGS_ADVANCED', 'UPDATE', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings: delete hub':['HUB_SETTINGS_ADVANCED', 'DELETE', ['hub_id'], [], {}, 'events', ''],
 
-  'Hub: create new post': ['HUB', 'CREATE??', ['post_id']], // TODO: correct?
-  'Hub: update post': ['HUB', 'UPDATE??', ['post_id']],
-  'Hub: delete post': ['HUB', 'DELETE??', ['post_id']],
+  'Hub settings design: view cover':['HUB_SETTINGS_DESIGN_COVER', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings design: view header':['HUB_SETTINGS_DESIGN_HEADER', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings design: view text':['HUB_SETTINGS_DESIGN_TEXT', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings design: update cover':['HUB_SETTINGS_DESIGN_COVER', 'UPDATE', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings design: update header':['HUB_SETTINGS_DESIGN_HEADER', 'UPDATE', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings design: update text':['HUB_SETTINGS_DESIGN_TEXT', 'UPDATE', ['hub_id'], [], {}, 'events', ''],
 
-  'Hub: visit a post': ['HUB_SPOTLIGHT', 'VIEW', ['hub_id','post_id']],
+  'Hub settings addons: view addons list':['HUB_SETTINGS_ADDONS', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings addons: view addon configuration':['HUB_SETTINGS_ADDONS', 'VIEW', ['hub_id', 'addon_config_id'], [], {}, 'events', ''],
+  'Hub settings addons: create addon configuration':['HUB_SETTINGS_ADDONS', 'CREATE', ['hub_id', 'addon_config_id'], [], {}, 'events', ''],
+  'Hub settings addons: update addon configuration':['HUB_SETTINGS_ADDONS', 'UPDATE', ['hub_id', 'addon_config_id'], [], {}, 'events', ''],
+  'Hub settings addons: delete addon configuration':['HUB_SETTINGS_ADDONS', 'DELETE', ['hub_id', 'addon_config_id'], [], {}, 'events', ''],
+  'Hub settings addons: enable addon configuration':['HUB_SETTINGS_ADDONS', 'PUBLISH', ['hub_id', 'addon_config_id'], [], {}, 'events', ''],
+  'Hub settings addons: disable addon configuration':['HUB_SETTINGS_ADDONS', 'UNPUBLISH', ['hub_id', 'addon_config_id'], [], {}, 'events', ''],
 
-  // Should be sent from pres.ly - in the case of https://pressly.com/salesforce/contenthub
-  'Hub: visit an external post from hub stream': ['EXTERNAL', 'VIEW', ['hub_id','post_id']],
+  'Hub settings collections: view collection list':['HUB_SETTINGS_COLLECTIONS', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings collections: view collection settings':['HUB_SETTINGS_COLLECTIONS', 'VIEW', ['hub_id', 'collection_id'], [], {}, 'events', ''],
+  'Hub settings collections: create collection':['HUB_SETTINGS_COLLECTIONS', 'CREATE', ['hub_id', 'collection_id'], [], {}, 'events', ''],
+  'Hub settings collections: update collection settings':['HUB_SETTINGS_ADDONS', 'UPDATE', ['hub_id', 'collection_id'], [], {}, 'events', ''],
+  'Hub settings collections: delete collection':['HUB_SETTINGS_ADDONS', 'DELETE', ['hub_id', 'collection_id'], [], {}, 'events', ''],  
 
-  'Hub: share a post from stream': ['HUB_STREAM', 'POST_SHARE', ['hub_id', 'post_id']],
-  'Hub: share a post from spotlight': ['HUB_SPOTLIGHT', 'POST_SHARE', ['hub_id', 'post_id']],
-  'Hub: schedule a share from spotlight': ['HUB_SPOTLIGHT', 'POST_SHARE???', ['hub_id', 'post_id']], // TODO, prob need POST_SHARE_SCHEDULE
+  'Hub settings CTAs: view CTA list':['HUB_SETTINGS_CTA', 'VIEW', ['hub_id'], [], {}, 'events', ''],
+  'Hub settings CTAs: view CTA settings':['HUB_SETTINGS_CTA', 'VIEW', ['hub_id', 'widget_id'], [], {}, 'events', ''],
+  'Hub settings CTAs: create CTA':['HUB_SETTINGS_CTA', 'CREATE', ['hub_id', 'widget_id'], [], {}, 'events', ''],
+  'Hub settings CTAs: update CTA settings':['HUB_SETTINGS_CTA', 'UPDATE', ['hub_id', 'widget_id'], [], {}, 'events', ''],
+  'Hub settings CTAs: delete CTA':['HUB_SETTINGS_CTA', 'DELETE', ['hub_id', 'widget_id'], [], {}, 'events', ''],
 
-  'Hub: like a post from stream': ['HUB_STREAM', 'POST_LIKE', ['post_id']],
-  'Hub: like a post from spotlight': ['HUB_SPOTLIGHT', 'POST_LIKE', ['post_id']],
-  'Hub: unlike a post from stream': ['HUB_STREAM', 'POST_UNLIKE', ['post_id']],
-  'Hub: unlike a post from spotlight': ['HUB_SPOTLIGHT', 'POST_UNLIKE', ['post_id']],
+  /* TODO: Integrations */
+  /*  
 
-  'Hub: comment on a post from spotlight': ['HUB_SPOTLIGHT', 'POST_COMMENT', ['post_id']],
-
+  
   'Hub>Newsletters: create a new newsletter': ['HUB_NEWSLETTERS', 'CREATE', ['hub_id']], // TODO...
   'Hub>Newsletters: send a newsletter': ['HUB_NEWSLETTERS', '???', ['hub_id']], // TODO: object_id?
   'Hub>Newsletters: delete a newsletter': ['HUB_NEWSLETTERS', 'DELETE', ['hub_id']], // TODO: object_id
@@ -124,24 +159,17 @@ export const actions = {
 
   'Org: view org page': ['ORG_PROFILE', 'VIEW', ['account_id']],
   'Org: edit settings': ['ORG_SETTINGS', 'UPDATE', ['account_id']],
-  'Org: view hubs': ['ORG_PROFILE', 'VIEW', []], // TODO: .. how to know they looked at "hubs" tab..?
+  'Org: view hubs': ['ORG_PROFILE', 'VIEW', []], // TODO: .. how to know they looked at 'hubs' tab..?
   'Org: view members': ['ORG_PROFILE', 'VIEW', []], // TODO ...?
   'Org: view posts': ['ORG_PROFILE', 'VIEW', []], // TODO ...? probably need ORG_PROFILE_POSTS etc..
 
   'User: view org page': ['ORG_PROFILE', 'VIEW', ['account_id']],
   'User: edit settings': ['ORG_SETTINGS', 'UPDATE', ['account_id']],
-  'User: view hubs': ['ORG_PROFILE', 'VIEW', []], // TODO: .. how to know they looked at "hubs" tab..?
+  'User: view hubs': ['ORG_PROFILE', 'VIEW', []], // TODO: .. how to know they looked at 'hubs' tab..?
   'User: view members': ['ORG_PROFILE', 'VIEW', []], // TODO ...?
   'User: view posts': ['ORG_PROFILE', 'VIEW', []], // TODO ...? probably need USER_PROFILE_POSTS etc..
 
-  // TODO: we don't actually know which social provider was most effective..
-  'Login via Password FAIL': ['LOGIN', 'PASSWORD_LOGIN_FAIL', []],
-  'Login via Password OK': ['LOGIN', 'PASSWORD_LOGIN_OK', []],
-  'Login via SSO FAIL': ['LOGIN', 'SSO_LOGIN_FAIL', []],
-  'Login via SSO OK': ['LOGIN', 'SSO_LOGIN_OK', []],
-  'Login via Social FAIL': ['LOGIN', 'SOCIAL_LOGIN_FAIL', []],
-  'Login via Social OK': ['LOGIN', 'SOCIAL_LOGIN_OK', []],
-  
+    
   // TODO: useful to understand the completion / incompletion of signups..
   'Signup: screen 1': ['SIGNUP', '???', []],
   'Signup: screen 2': ['SIGNUP', '???', []],
