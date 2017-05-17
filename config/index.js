c711dcb6f71648bdf5824e171dc91d1977d4d27a
Ali Najafizadeh <a.najafizadeh@gmail.com>
2017-05-17 14:49:47 -0400

    added a new configuration to generate umd build

--- config/index.js
+++ config/index.js
@@ -1,10 +1,16 @@
 /* eslint no-undef: 0 */
 import webpackConfigDefault from './webpack.config'
 import webpackConfigDist from './webpack.config.dist'
+import webpackConfigUmdDist from './webpack.config.umd.dist'
 
 const config = {}
 if (process.env.NODE_ENV == 'production') {
-  config.webpack = webpackConfigDist
+  config.webpack = {
+    build: function () {
+      webpackConfigDist.build()
+      webpackConfigUmdDist.build()
+    }
+  }
 } else {
   config.webpack = webpackConfigDefault
 }
---
+++ config/webpack.config.umd.dist.js
@@ -0,0 +1,52 @@
+/* eslint no-console: 0, no-undef: 0 */
+import path from 'path'
+import webpack from 'webpack'
+import { ROOT_PATH, SRC_PATH } from './constants'
+
+const config = {
+  entry: [
+    'babel-polyfill', 
+    path.join(SRC_PATH, 'index.js')
+  ],
+
+  output: {
+    path: './dist',
+    filename: 'browser-metrix.js',
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: process.env.NODE_ENV !== 'production',
+      'process.env': JSON.stringify(process.env)
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      comments: false,
+      mangle: true,
+      minimize: true
+    })
+  ]
+}
+
+// Dist webpack config is to compile source to dist/
+config.build = () => {
+  const compiler = webpack(config)
+  compiler.run((err) => {
+    if (err) {
+      throw err
+    }
+  })
+}
+
+export default config
