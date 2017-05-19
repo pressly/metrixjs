9609370a045291aa033fcde72375599398ed0f87
Ali Najafizadeh <a.najafizadeh@gmail.com>
2017-05-19 15:32:08 -0400

    fixed metrix-web to use babel-polyfill

--- config/webpack.config.dist.js
+++ config/webpack.config.dist.js
@@ -5,17 +5,17 @@ import { ROOT_PATH, SRC_PATH } from './constants'
 
 const config = {
   entry: {
-    metrix: path.join(SRC_PATH, 'index.js'),
-    'metrix-with-polyfill': [
-      'babel-polyfill', 
-      path.join(SRC_PATH, 'index.js')
-    ]
+    'metrix': path.join(SRC_PATH, 'index.js'),
+    // uncomment the below line if you need to release it for web with babel-polyfill
+    // we can't run both these entries together. we have to run one at the time.
+    //'metrix-web': path.join(SRC_PATH, 'web.js')
   },
 
   output: {
     path: './dist',
     filename: '[name].js',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
+    library: 'metrixjs'
   },
 
   module: {
---
+++ src/web.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill'
+import MetrixExport, { Metrix, MetrixNoop } from './index'
+
+export {
+  Metrix,
+  MetrixNoop
+}
+
+export default MetrixExport
